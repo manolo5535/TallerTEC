9d90959a8984b93b3cbf8a9b5ca92bb2d0628a1e
manolo5535 <manolos5535@hotmail.com>
2022-09-16 06:40:44 -0600

    Funciones controlador

--- src/main/java/com/TallerTEC/TallerTEC/Controller/Controlador.java
+++ src/main/java/com/TallerTEC/TallerTEC/Controller/Controlador.java
@@ -1,7 +1,19 @@
 package com.TallerTEC.TallerTEC.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.TallerTEC.TallerTEC.Entidades.*;
+
 public class Controlador {
 	
+	List<Cliente> listaC = new ArrayList<Cliente>();
+	
+public List<Cliente> listaCliente(){
+	
+	return listaC;
+
+}
 
 
 }
